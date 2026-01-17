1e92c5ef369082c1d31f7d97455ff20c866f6fbd
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2026-01-17 19:24:54 +0400

    feat(rate-limiters): add rate limiter for image uploads to manage request frequency

--- src/middlewares/rate-limiters/question.rate-limiters.ts
+++ src/middlewares/rate-limiters/question.rate-limiters.ts
@@ -59,6 +59,13 @@ const rollbackVersionLimiter = new RateLimiterRedis({
   duration: 60 * 30,
 });
 
+const validateContentImageLimiter = new RateLimiterRedis({
+  storeClient: getRedisMessagingClient(),
+  keyPrefix: "validateContentImage",
+  points: 15,
+  duration: 60 * 60,
+});
+
 const createQuestionLimiterMiddleware = createRateLimiterMiddleware(
   createQuestionLimiter,
   "Too many questions created, try again after half an hour",
@@ -99,6 +106,11 @@ const rollbackVersionLimiterMiddleware = createRateLimiterMiddleware(
   "Too many rollbacks, try again later",
 );
 
+const validateContentImageMiddleware = createRateLimiterMiddleware(
+  validateContentImageLimiter,
+  "Too many image uploads, try again later",
+);
+
 export {
   createQuestionLimiterMiddleware,
   createAnswerOnQuestionLimiterMiddleware,
@@ -108,4 +120,5 @@ export {
   markAnswerAsBestLimiterMiddleware,
   editQuestionLimiterMiddleware,
   rollbackVersionLimiterMiddleware,
+  validateContentImageMiddleware,
 };
