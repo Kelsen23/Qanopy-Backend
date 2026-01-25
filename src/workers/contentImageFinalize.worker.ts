1112166a4469388d89c14356a688ea4b1f09d895
Kelsen23 <141734807+Kelsen23@users.noreply.github.com>
2026-01-25 15:12:33 +0400

    feat(contentImageFinalize.worker): implement worker for finalizing content images in the contentImageFinalizeQueue

---
+++ src/workers/contentImageFinalize.worker.ts
@@ -0,0 +1,77 @@
+import { Worker } from "bullmq";
+import { redisMessagingClientConnection } from "../config/redis.config.js";
+
+import { cloudfrontDomain } from "../config/s3.config.js";
+
+import HttpError from "../utils/httpError.util.js";
+
+import getObjectKeyFromUrl from "../utils/getObjectKeyFromUrl.util.js";
+import moveS3Object from "../utils/moveS3Object.util.js";
+
+import connectMongoDB from "../config/mongodb.config.js";
+
+import Question from "../models/question.model.js";
+import Answer from "../models/answer.model.js";
+
+import crypto from "crypto";
+
+async function startWorker() {
+  await connectMongoDB(process.env.MONGO_URI as string);
+  console.log("Mongo connected, starting content image finalization worker...");
+
+  new Worker(
+    "contentImageFinalizeQueue",
+    async (job) => {
+      const { entityType, entityId } = job.data;
+
+      const entity =
+        entityType === "question"
+          ? await Question.findById(entityId).select("body")
+          : await Answer.findById(entityId).select("body");
+
+      if (!entity) throw new HttpError("Content not found", 404);
+
+      let newBody = entity.body as string;
+
+      const TEMP_IMAGE_REGEX =
+        /https:\/\/[^/]+\/temp\/[a-zA-Z0-9/_-]+\.(png|jpg|jpeg|webp)/gi;
+      const tempImageUrls = new Set(
+        (entity.body as string).match(TEMP_IMAGE_REGEX) || [],
+      );
+
+      for (const url of tempImageUrls) {
+        try {
+          if (!(url as string).includes("/temp/")) continue;
+
+          const fromKey = getObjectKeyFromUrl(url as string);
+
+          const newKey = `content/${entityType}s/${entityId}/${crypto.randomUUID()}.png`;
+
+          await moveS3Object(fromKey, newKey);
+
+          const newUrl = `${cloudfrontDomain}/${newKey}`;
+
+          newBody = newBody.replaceAll(url, newUrl);
+        } catch (error) {
+          console.error("Failed to process image", { url, error });
+        }
+      }
+
+      if (entity.body !== newBody) {
+        await (entityType === "question" ? Question : Answer).findByIdAndUpdate(
+          entityId,
+          { body: newBody },
+        );
+      }
+    },
+    {
+      connection: redisMessagingClientConnection,
+      concurrency: 1,
+    },
+  );
+}
+
+startWorker().catch((error) => {
+  console.error("Failed to start content image finalization worker:", error);
+  process.exit(1);
+});
