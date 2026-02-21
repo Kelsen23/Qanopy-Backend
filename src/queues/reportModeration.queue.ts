274a5590c9ab9268123d4ef241f2d05a2fefd6f5
Kelsen23 <nikolozigoreziani@gmail.com>
2026-02-21 21:10:34 +0400

    chore(reportModeration.queue): remove unused reportModerationQueue file

--- src/queues/reportModeration.queue.ts
+++
@@ -1,8 +0,0 @@
-import { Queue } from "bullmq";
-import { redisMessagingClientConnection } from "../config/redis.config.js";
-
-const reportModerationQueue = new Queue("reportModerationQueue", {
-  connection: redisMessagingClientConnection,
-});
-
-export default reportModerationQueue;
