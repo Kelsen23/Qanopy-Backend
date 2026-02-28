ac38a286024c0896a1c4431124ef98dcfa0335cd
Kelsen23 <nikolozigoreziani@gmail.com>
2026-02-28 19:07:49 +0400

    feat(graphql resolvers): add moderation resolver to the merged resolvers

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -2,7 +2,12 @@ import { mergeResolvers } from "@graphql-tools/merge";
 
 import userResolver from "./user.resolver.js";
 import questionResolver from "./question.resolver.js";
+import moderationResolver from "./moderation.resolver.js";
 
-const resolvers = mergeResolvers([userResolver, questionResolver]);
+const resolvers = mergeResolvers([
+  userResolver,
+  questionResolver,
+  moderationResolver,
+]);
 
 export default resolvers;
