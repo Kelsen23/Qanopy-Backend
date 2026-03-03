e165d9be68f3f81aa3424258b1f1d842629d659c
Kelsen23 <nikolozigoreziani@gmail.com>
2026-03-03 10:18:52 +0400

    feat(modActionLog.model): enhance schema by adding decisionId, actorType, and new fields for overridden actions

--- src/models/modActionLog.model.ts
+++ src/models/modActionLog.model.ts
@@ -2,30 +2,34 @@ import mongoose, { Schema } from "mongoose";
 
 const ModActionLogSchema: Schema = new Schema(
   {
+    decisionId: { type: String, required: true },
+
     targetType: {
       type: String,
-      enum: ["Report", "Strike"],
+      enum: ["Content", "User", "Report", "Strike"],
       required: true,
     },
-
     targetId: {
       type: String,
       required: true,
     },
-
-    adminId: {
-      type: String,
-      required: true,
-    },
-
     targetUserId: {
       type: String,
       required: true,
     },
 
+    actorType: {
+      type: String,
+      enum: ["ADMIN_MODERATION", "AI_MODERATION"],
+      required: true,
+    },
+    adminId: {
+      type: String,
+    },
+
     actionTaken: {
       type: String,
-      enum: ["BAN_TEMP", "BAN_PERM", "WARN", "IGNORE"],
+      enum: ["BAN_TEMP", "BAN_PERM", "WARN", "REMOVE", "IGNORE"],
       required: true,
     },
 
@@ -33,6 +37,9 @@ const ModActionLogSchema: Schema = new Schema(
       type: mongoose.Schema.Types.Mixed,
       default: {},
     },
+
+    overriddenBy: { type: String },
+    overriddenAt: { type: Date },
   },
   {
     timestamps: true,
