090a446f6f8ee96ce76887af692b2320bb128c48
Kelsen23 <nikolozigoreziani@gmail.com>
2026-03-04 15:17:32 +0400

    feat(notification): add queueNotification utility for managing notification jobs

---
+++ src/utils/queueNotification.util.ts
@@ -0,0 +1,32 @@
+import notificationQueue from "../queues/notification.queue.js";
+
+type NotificationType =
+  | "WARN"
+  | "STRIKE"
+  | "REPORT_UPDATE"
+  | "REMOVE_CONTENT"
+  | "UPVOTE"
+  | "ANSWER"
+  | "MENTION"
+  | "REPLY";
+
+const queueNotification = async ({
+  userId,
+  type,
+  referenceId,
+  meta,
+}: {
+  userId: string;
+  type: NotificationType;
+  referenceId: string;
+  meta: Record<string, unknown>;
+}) => {
+  await notificationQueue.add("createNotification", {
+    userId,
+    type,
+    referenceId,
+    meta,
+  });
+};
+
+export default queueNotification;
