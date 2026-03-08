677de49310049ebf83e4b1ea275e05a39db4f040
Kelsen23 <nikolozigoreziani@gmail.com>
2026-03-08 12:22:53 +0400

    feat(deleteContent): initialize delete content worker with MongoDB connection and error handling

--- src/workers/deleteContent.worker.ts
+++ src/workers/deleteContent.worker.ts
@@ -2,18 +2,30 @@ import { Worker } from "bullmq";
 
 import { redisMessagingClientConnection } from "../config/redis.config.js";
 
+import connectMongoDB from "../config/mongodb.config.js";
+
 import deleteContent from "../services/question/deleteContent.service.js";
 
-new Worker(
-  "deleteContentQueue",
-  async (job) => {
-    const { userId, targetType, targetId } = job.data;
+async function startWorker() {
+  await connectMongoDB(process.env.MONGO_URI as string);
+  console.log("Mongo connected, starting delete content worker...");
 
-    await deleteContent(userId, targetType.toLowerCase(), targetId);
-  },
-  {
-    connection: redisMessagingClientConnection,
-    concurrency: 1,
-    limiter: { max: 5, duration: 5000 },
-  },
-);
+  new Worker(
+    "deleteContentQueue",
+    async (job) => {
+      const { userId, targetType, targetId } = job.data;
+
+      await deleteContent(userId, targetType.toLowerCase(), targetId);
+    },
+    {
+      connection: redisMessagingClientConnection,
+      concurrency: 1,
+      limiter: { max: 5, duration: 5000 },
+    },
+  );
+}
+
+startWorker().catch((error) => {
+  console.error("Failed to start delete content worker:", error);
+  process.exit(1);
+});
