d9321998fbe2c724bb73a26079250407de4cc13c
Kelsen23 <nikolozigoreziani@gmail.com>
2026-03-14 20:38:44 +0400

    feat(question): enhance convertQuestionToText utility to include optional tags parameter

--- src/utils/convertQuestionToText.util.ts
+++ src/utils/convertQuestionToText.util.ts
@@ -1,5 +1,9 @@
-const convertQuestionToText = (title: string, body: string) => {
-  return `Title: ${title}\nBody: ${body.slice(0, 300)}`;
+const convertQuestionToText = (
+  title: string,
+  body: string,
+  tags?: string[],
+) => {
+  return `Title: ${title}\nBody: ${body.slice(0, 300)}\nTags: ${tags ? `${tags.slice(0, 10).join(", ")}` : ""}`;
 };
 
 export default convertQuestionToText;
