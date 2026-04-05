37c8e16408b93570c704fb18b94c73fa1e0ab7a2
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-05 13:27:14 +0400

    refactor(answer.model, question.model, reply.model): remove upvote and downvote properties from transformed output for cleaner response structure

--- src/models/reply.model.ts
+++ src/models/reply.model.ts
@@ -27,12 +27,7 @@ const ReplySchema: Schema = new Schema(
       versionKey: false,
       transform: (_, ret: any) => {
         ret.id = ret._id;
-        ret.upvotes = ret.upvoteCount;
-        ret.downvotes = ret.downvoteCount;
-
         delete ret._id;
-        delete ret.upvoteCount;
-        delete ret.downvoteCount;
 
         return ret;
       },
