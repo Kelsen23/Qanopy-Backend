4035fead7a22d675a33f510dd1de4307ffa27d84
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-12 14:20:23 +0400

    refactor(worker): streamline contentPipelineRouter by integrating dedicated services for question, answer, reply, and AI answer feedback processing

--- src/workers/contentPipelineRouter.worker.ts
+++ src/workers/contentPipelineRouter.worker.ts
@@ -3,14 +3,10 @@ import { Worker } from "bullmq";
 import { redisMessagingClientConnection } from "../config/redis.config.js";
 import connectMongoDB from "../config/mongodb.config.js";
 
-import QuestionVersion from "../models/questionVersion.model.js";
-
-import contentModerationQueue from "../queues/contentModeration.queue.js";
-import topicDeterminationQueue from "../queues/topicDetermination.queue.js";
-import questionEmbeddingQueue from "../queues/questionEmbedding.queue.js";
-import similarQuestionsQueue from "../queues/similarQuestions.queue.js";
-
-import { makeJobId } from "../utils/makeJobId.util.js";
+import questionPipelineRouterService from "../services/question/pipelineRouters/questionPipelineRouter.service.js";
+import answerPipelineRouterService from "../services/question/pipelineRouters/answerPipelineRouter.service.js";
+import replyPipelineRouterService from "../services/question/pipelineRouters/replyPipelineRouter.service.js";
+import aiAnswerFeedbackPipelineRouterService from "../services/question/pipelineRouters/aiAnswerFeedbackPipelineRouter.service.js";
 
 async function startWorker() {
   await connectMongoDB(process.env.MONGO_URI as string);
@@ -19,93 +15,28 @@ async function startWorker() {
   const worker = new Worker(
     "contentPipelineRouter",
     async (job) => {
-      const { questionId, version } = job.data;
+      const contentType = job.name as
+        | "QUESTION"
+        | "ANSWER"
+        | "REPLY"
+        | "AI_ANSWER_FEEDBACK";
+      const { contentId, version } = job.data;
 
-      const foundQuestionVersion = await QuestionVersion.findOne({
-        questionId,
-        version,
-      }).select(
-        "moderationStatus topicStatus embeddingStatus similarQuestionIds",
-      );
-
-      if (!foundQuestionVersion) return;
-
-      if (foundQuestionVersion.moderationStatus === "PENDING") {
-        return await contentModerationQueue.add(
-          "QUESTION",
-          {
-            contentId: questionId,
-            version,
-          },
-          {
-            removeOnComplete: true,
-            removeOnFail: false,
-            jobId: makeJobId(
-              "contentModeration",
-              "QUESTION",
-              questionId,
-              version,
-            ),
-          },
-        );
-      } else if (foundQuestionVersion.moderationStatus === "REJECTED") {
-        return;
-      } else if (foundQuestionVersion.topicStatus === "PENDING") {
-        return await topicDeterminationQueue.add(
-          "QUESTION",
-          {
-            questionId,
-            version,
-          },
-          {
-            removeOnComplete: true,
-            removeOnFail: false,
-            jobId: makeJobId(
-              "topicDetermination",
-              "QUESTION",
-              questionId,
-              version,
-            ),
-          },
-        );
-      } else if (
-        foundQuestionVersion.topicStatus === "VALID" &&
-        ["NONE", "PENDING"].includes(String(foundQuestionVersion.embeddingStatus))
-      ) {
-        return await questionEmbeddingQueue.add(
-          "EMBED_QUESTION",
-          {
-            questionId,
-            version,
-          },
-          {
-            removeOnComplete: true,
-            removeOnFail: false,
-            jobId: makeJobId(
-              "questionEmbedding",
-              "EMBED_QUESTION",
-              questionId,
-              version,
-            ),
-          },
-        );
-      } else if (
-        foundQuestionVersion.topicStatus === "VALID" &&
-        foundQuestionVersion.embeddingStatus === "READY" &&
-        Array.isArray(foundQuestionVersion.similarQuestionIds) &&
-        (foundQuestionVersion.similarQuestionIds as Array<string>).length === 0
-      ) {
-        return await similarQuestionsQueue.add(
-          "SEARCH_SIMILAR_QUESTIONS",
-          {
-            questionId,
-          },
-          {
-            removeOnComplete: true,
-            removeOnFail: false,
-            jobId: makeJobId("similarQuestions", questionId),
-          },
-        );
+      switch (contentType) {
+        case "QUESTION":
+          await questionPipelineRouterService(contentId, version);
+          break;
+        case "ANSWER":
+          await answerPipelineRouterService(contentId);
+          break;
+        case "REPLY":
+          await replyPipelineRouterService(contentId);
+          break;
+        case "AI_ANSWER_FEEDBACK":
+          await aiAnswerFeedbackPipelineRouterService(contentId);
+          break;
+        default:
+          break;
       }
     },
     {
