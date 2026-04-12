51e6cf7f003676d6db11abe054e3b7b76b1c25de
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-12 14:19:23 +0400

    feat(service): implement questionPipelineRouter to manage question processing through various queues based on moderation and topic statuses

---
+++ src/services/question/pipelineRouters/questionPipelineRouter.service.ts
@@ -0,0 +1,87 @@
+import { makeJobId } from "../../../utils/makeJobId.util.js";
+
+import QuestionVersion from "../../../models/questionVersion.model.js";
+
+import contentModerationQueue from "../../../queues/contentModeration.queue.js";
+import topicDeterminationQueue from "../../../queues/topicDetermination.queue.js";
+import questionEmbeddingQueue from "../../../queues/questionEmbedding.queue.js";
+import similarQuestionsQueue from "../../../queues/similarQuestions.queue.js";
+
+const questionPipelineRouter = async (questionId: string, version: number) => {
+  const foundQuestionVersion = await QuestionVersion.findOne({
+    questionId: questionId,
+    version,
+  }).select("moderationStatus topicStatus embeddingStatus similarQuestionIds");
+
+  if (!foundQuestionVersion) return;
+
+  if (foundQuestionVersion.moderationStatus === "PENDING") {
+    return await contentModerationQueue.add(
+      "QUESTION",
+      {
+        questionId,
+        version,
+      },
+      {
+        removeOnComplete: true,
+        removeOnFail: false,
+        jobId: makeJobId("contentModeration", "QUESTION", questionId, version),
+      },
+    );
+  } else if (foundQuestionVersion.moderationStatus === "REJECTED") {
+    return;
+  } else if (foundQuestionVersion.topicStatus === "PENDING") {
+    return await topicDeterminationQueue.add(
+      "QUESTION_TOPIC",
+      {
+        questionId: questionId,
+        version,
+      },
+      {
+        removeOnComplete: true,
+        removeOnFail: false,
+        jobId: makeJobId("topicDetermination", "QUESTION", questionId, version),
+      },
+    );
+  } else if (
+    foundQuestionVersion.topicStatus === "VALID" &&
+    ["NONE", "PENDING"].includes(String(foundQuestionVersion.embeddingStatus))
+  ) {
+    return await questionEmbeddingQueue.add(
+      "QUESTION_EMBEDDING",
+      {
+        questionId: questionId,
+        version,
+      },
+      {
+        removeOnComplete: true,
+        removeOnFail: false,
+        jobId: makeJobId(
+          "questionEmbedding",
+          "QUESTION_EMBEDDING",
+          questionId,
+          version,
+        ),
+      },
+    );
+  } else if (
+    foundQuestionVersion.topicStatus === "VALID" &&
+    foundQuestionVersion.embeddingStatus === "READY" &&
+    Array.isArray(foundQuestionVersion.similarQuestionIds) &&
+    (foundQuestionVersion.similarQuestionIds as Array<string>).length === 0
+  ) {
+    return await similarQuestionsQueue.add(
+      "QUESTION_SIMILARITY",
+      {
+        questionId,
+      },
+      {
+        removeOnComplete: true,
+        removeOnFail: false,
+        jobId: makeJobId("similarQuestions", questionId),
+      },
+    );
+  }
+};
+
+export default questionPipelineRouter;
