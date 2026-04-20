4ac48dd309df9f8a19a313dc98cdb96a327bc424
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-20 16:24:38 +0400

    feat(user.typeDefs): add Notification type and related enums for enhanced user notifications

--- src/graphql/typeDefs/user.typeDefs.ts
+++ src/graphql/typeDefs/user.typeDefs.ts
@@ -1,12 +1,6 @@
 import { gql } from "graphql-tag";
 
 const userTypeDefs = gql`
-  enum Role {
-    ADMIN
-    MOD
-    USER
-  }
-
   type Achievement {
     id: String!
     userId: String!
@@ -33,8 +27,101 @@ const userTypeDefs = gql`
     createdAt: String!
   }
 
+  type Notification {
+    id: String!
+
+    recipientId: String!
+    actorId: String
+    actor: User
+
+    event: NotificationEvent!
+
+    target: NotificationTarget!
+
+    meta: JSON!
+
+    seen: Boolean!
+
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type NotificationTarget {
+    entityType: NotificationEntityType!
+    entityId: String!
+    parentId: String
+  }
+
+  # Connections
+
+  type NotificationConnection {
+    notifications: [Notification!]!
+    nextCursor: NotificationCursor
+    hasMore: Boolean!
+    unreadCount: Int!
+  }
+
+  # Enums
+
+  enum Role {
+    ADMIN
+    MOD
+    USER
+  }
+
+  enum NotificationEvent {
+    UPVOTE
+    DOWNVOTE
+
+    ANSWER_CREATED
+    REPLY_CREATED
+
+    ANSWER_ACCEPTED
+    ANSWER_MARKED_BEST
+
+    AI_SUGGESTION_UNLOCKED
+    AI_ANSWER_UNLOCKED
+
+    SIMILAR_QUESTIONS_READY
+    AI_SUGGESTION_READY
+    AI_ANSWER_READY
+
+    WARN
+    STRIKE
+    REPORT_UPDATE
+    REMOVE_CONTENT
+  }
+
+  enum NotificationEntityType {
+    QUESTION
+    ANSWER
+    REPLY
+    AI_ANSWER_FEEDBACK
+    REPORT
+    USER
+  }
+
+  # Cursor Outputs
+
+  type NotificationCursor {
+    id: String!
+    createdAt: String!
+  }
+
+  # Cursor Inputs
+
+  input NotificationCursorInput {
+    id: String!
+    createdAt: String!
+  }
+
   extend type Query {
     user(id: String!): User
+
+    notifications(
+      cursor: NotificationCursorInput
+      limitCount: Int
+    ): NotificationConnection
   }
 `;
 
