d54aa8a1f886b5f03b3fa5e5533436f1a5a23d12
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-28 21:51:15 +0400

    feat(queueUserInterest): enhance job queuing with unique job IDs and completion options

--- src/utils/queueUserInterest.util.ts
+++ src/utils/queueUserInterest.util.ts
@@ -1,5 +1,7 @@
 import userInterestQueue from "../queues/userInterest.queue.js";
 
+import { makeUniqueJobId } from "./makeJobId.util.js";
+
 export type UserInterestAction = "VIEW" | "UPVOTE" | "ANSWER";
 
 type QueueUserInterestParams = {
@@ -13,10 +15,18 @@ const queueUserInterest = async ({
   tags,
   action,
 }: QueueUserInterestParams) => {
-  await userInterestQueue.add(action, {
-    userId,
-    tags,
-  });
+  await userInterestQueue.add(
+    action,
+    {
+      userId,
+      tags,
+    },
+    {
+      removeOnComplete: true,
+      removeOnFail: false,
+      jobId: makeUniqueJobId("userInterest", action, userId, ...tags),
+    },
+  );
 };
 
 export default queueUserInterest;
