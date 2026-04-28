5a34c1fee38c1c621216e5fa28da544b853be92c
Kelsen23 <nikolozigoreziani@gmail.com>
2026-04-28 22:51:04 +0400

    refactor(prisma.schema): remove interests field from User model and delete reset migration files to clean it up

--- prisma/migrations/20260404070127_add_ai_answer_feedback_content_type_enum/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterEnum
-ALTER TYPE "ContentType" ADD VALUE 'AI_ANSWER_FEEDBACK';
-
--- AlterTable
-ALTER TABLE "ModerationStrike" ALTER COLUMN "targetContentVersion" DROP NOT NULL;
--- prisma/migrations/20260420071239_add_rows_in_notification_settings_model/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "NotificationSettings" ADD COLUMN     "answerAccepted" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "answerMarkedBest" BOOLEAN NOT NULL DEFAULT true;
