7585cd6a84c762014ecf163f18ad2deb4c437aa6
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-15 21:37:26 +0400

    feat(generateToken.util): include tokenVersion when generating jwt token

--- src/utils/generateToken.util.ts
+++ src/utils/generateToken.util.ts
@@ -1,10 +1,14 @@
 import { Response } from "express";
 import jwt from "jsonwebtoken";
 
-const generateToken = (res: Response, userId: string) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
-    expiresIn: "7d",
-  });
+const generateToken = (res: Response, userId: string, tokenVersion: number) => {
+  const token = jwt.sign(
+    { userId, tokenVersion },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "7d",
+    },
+  );
 
   res.cookie("token", token, {
     httpOnly: true,
