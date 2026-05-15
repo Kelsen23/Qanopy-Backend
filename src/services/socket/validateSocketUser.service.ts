34b1f0d2058e40f204ad684344b0708768cd8ba7
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-15 21:36:40 +0400

    feat(validateSocketUser.service): add tokenVersion validation in the service to prevent expired tokens authenticating to socket

--- src/services/socket/validateSocketUser.service.ts
+++ src/services/socket/validateSocketUser.service.ts
@@ -3,25 +3,37 @@ import { getRedisCacheClient } from "../../config/redis.config.js";
 
 type SocketConnectUser = {
   id: string;
+  tokenVersion: number;
   status: string;
   isVerified: boolean;
+  isDeleted?: boolean;
 };
 
-const validateSocketUser = async (userId: string) => {
+const validateSocketUser = async (userId: string, tokenVersion: number) => {
   const cachedUser = await getRedisCacheClient().get(`user:${userId}`);
 
   const user: SocketConnectUser | null = cachedUser
     ? (JSON.parse(cachedUser) as SocketConnectUser)
     : await prisma.user.findUnique({
         where: { id: userId },
-        select: { id: true, status: true, isVerified: true },
+        select: {
+          id: true,
+          tokenVersion: true,
+          status: true,
+          isVerified: true,
+          isDeleted: true,
+        },
       });
 
   if (!user) {
     throw new Error("User not found");
   }
 
-  if (!user.isVerified || user.status !== "ACTIVE") {
+  if (Number(user.tokenVersion ?? 0) !== Number(tokenVersion ?? 0)) {
+    throw new Error("User token expired");
+  }
+
+  if (!user.isVerified || user.status !== "ACTIVE" || user.isDeleted) {
     throw new Error("Unauthorized user");
   }
 };
