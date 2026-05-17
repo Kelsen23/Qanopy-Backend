0dc87f242620aa233464bdcf72d764f7213f8551
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-17 13:51:15 +0400

    refactor(auth.route): format, clean-up and make the route names follow the clean, consistent pattern

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -17,38 +17,42 @@ import {
 
 import isAuthenticated from "../middlewares/auth.middleware.js";
 import validate from "../middlewares/validate.middleware.js";
-import {
-  loginSchema,
-  oauthSchema,
-  registerSchema,
-  verifyEmailSchema,
-  sendResetPasswordEmailSchema,
-  verifyResetPasswordOtpSchema,
-  resetPasswordSchema,
-  changePasswordSchema,
-} from "../validations/auth.schema.js";
 
 import {
   emailVerificationLimiterMiddleware,
   loginLimiterMiddleware,
+  oauthLimiterMiddleware,
   registerLimiterMiddleware,
   resendEmailLimiterMiddleware,
-  resetPasswordLimiterMiddleware,
-  changePasswordLimiterMiddleware,
-  generalLimiterMiddleware,
+  passwordResetLimiterMiddleware,
+  passwordChangeLimiterMiddleware,
+  sessionLimiterMiddleware,
 } from "../middlewares/rate-limiters/auth.rate-limiters.js";
+
+import {
+  changePasswordSchema,
+  loginSchema,
+  oauthSchema,
+  registerSchema,
+  resetPasswordSchema,
+  sendResetPasswordEmailSchema,
+  verifyEmailSchema,
+  verifyResetPasswordOtpSchema,
+} from "../validations/auth.schema.js";
 
 const router = express.Router();
 
 router
   .route("/register")
   .post(registerLimiterMiddleware, validate(registerSchema), register);
+
 router
   .route("/login")
   .post(loginLimiterMiddleware, validate(loginSchema), login);
+
 router
-  .route("/registerOrLogin")
-  .post(registerLimiterMiddleware, validate(oauthSchema), registerOrLogin);
+  .route("/oauth")
+  .post(oauthLimiterMiddleware, validate(oauthSchema), registerOrLogin);
 
 router
   .route("/email/verify")
@@ -58,35 +62,39 @@ router
     validate(verifyEmailSchema),
     verifyEmail,
   );
+
 router
-  .route("/verificationEmail/resend")
+  .route("/email/resend")
   .post(resendEmailLimiterMiddleware, isAuthenticated, resendVerificationEmail);
 
 router
-  .route("/resetPasswordEmail/send")
+  .route("/password/reset/send")
   .post(
-    resetPasswordLimiterMiddleware,
+    passwordResetLimiterMiddleware,
     validate(sendResetPasswordEmailSchema),
     sendResetPasswordEmail,
   );
+
 router
-  .route("/resetPasswordEmail/resend")
+  .route("/password/reset/resend")
   .post(
     resendEmailLimiterMiddleware,
     validate(sendResetPasswordEmailSchema),
     resendResetPasswordEmail,
   );
+
 router
-  .route("/resetPasswordOtp/verify")
+  .route("/password/reset/verify")
   .post(
     emailVerificationLimiterMiddleware,
     validate(verifyResetPasswordOtpSchema),
     verifyResetPasswordOtp,
   );
+
 router
   .route("/password/reset")
   .post(
-    resetPasswordLimiterMiddleware,
+    passwordResetLimiterMiddleware,
     validate(resetPasswordSchema),
     resetPassword,
   );
@@ -94,14 +102,16 @@ router
 router
   .route("/password/change")
   .post(
-    changePasswordLimiterMiddleware,
+    passwordChangeLimiterMiddleware,
     isAuthenticated,
     validate(changePasswordSchema),
     changePassword,
   );
 
-router.route("/isAuth").get(generalLimiterMiddleware, isAuthenticated, isAuth);
+router.route("/session").get(sessionLimiterMiddleware, isAuthenticated, isAuth);
 
-router.route("/logout").post(generalLimiterMiddleware, isAuthenticated, logout);
+router
+  .route("/session")
+  .post(sessionLimiterMiddleware, isAuthenticated, logout);
 
 export default router;
