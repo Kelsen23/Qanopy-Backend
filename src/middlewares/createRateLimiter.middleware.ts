32bb9342634ebad3036bf7f1aa143b51fcadc6a4
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-17 14:16:34 +0400

    feat(createRateLimiter.middleware): add keyResolver parameter that lets req.user.id be consumed with redis limiter

--- src/middlewares/createRateLimiter.middleware.ts
+++ src/middlewares/createRateLimiter.middleware.ts
@@ -2,13 +2,16 @@ import { RateLimiterRedis } from "rate-limiter-flexible";
 
 import { NextFunction, Request, Response } from "express";
 
+type RateLimiterKeyResolver = (req: Request) => string;
+
 const createRateLimiterMiddleware = (
   limiter: RateLimiterRedis,
   message: string,
+  keyResolver: RateLimiterKeyResolver = (req) => req.ip || "unknown",
 ) => {
   return (req: Request, res: Response, next: NextFunction) => {
     limiter
-      .consume(req.ip || "unknown")
+      .consume(keyResolver(req))
       .then(() => next())
       .catch(() => {
         res.status(429).json({ message });
