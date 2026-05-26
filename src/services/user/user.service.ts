51ee118ff2f824e9c47a9b657f7c591b8ab74ab7
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-26 14:37:15 +0400

    feat(user.service): create a place to export user http services

---
+++ src/services/user/user.service.ts
@@ -0,0 +1,7 @@
+export { default as updateProfilePicture } from "./requestProfilePictureUpdate.service.js";
+export { default as deleteProfilePicture } from "./deleteProfilePicture.service.js";
+export { default as updateProfile } from "./updateProfile.service.js";
+export { default as deleteAccount } from "./deleteAccount.service.js";
+export { default as getNotificationSettings } from "./getNotificationSettings.service.js";
+export { default as updateNotificationSettings } from "./updateNotificationSettings.service.js";
+export { default as markNotificationsAsSeen } from "./markNotificationsAsSeen.service.js";
