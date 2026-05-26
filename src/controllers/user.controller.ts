54b470ce007f2a6b48d256f5e485da49ddb66301
Kelsen23 <nikolozigoreziani@gmail.com>
2026-05-26 18:47:16 +0400

    feat(user.controller): add and implement resendEmailChange, sendEmailChange, verifyEmailChange services into their own controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,14 +4,19 @@ import asyncHandler from "../middlewares/asyncHandler.middleware.js";
 
 import AuthenticatedRequest from "../types/authenticatedRequest.type.js";
 
+import getDeviceInfo from "../utils/getDeviceInfo.util.js";
+
 import {
   deleteAccount as deleteAccountService,
   deleteProfilePicture as deleteProfilePictureService,
   getNotificationSettings as getNotificationSettingsService,
   markNotificationsAsSeen as markNotificationsAsSeenService,
+  resendEmailChange as resendEmailChangeService,
+  sendEmailChange as sendEmailChangeService,
   updateNotificationSettings as updateNotificationSettingsService,
   updateProfile as updateProfileService,
   updateProfilePicture as updateProfilePictureService,
+  verifyEmailChange as verifyEmailChangeService,
 } from "../services/user/user.service.js";
 
 const updateProfilePicture = asyncHandler(
@@ -114,6 +119,79 @@ const markNotificationsAsSeen = asyncHandler(
   },
 );
 
+const sendEmailChange = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { id: userId } = req.user;
+    const { newEmail } = req.body;
+    const deviceInfo = getDeviceInfo(req);
+
+    const {
+      emailChangeOtpExpireAt,
+      emailChangeOtpResendAvailableAt,
+      pendingEmail,
+    } = await sendEmailChangeService({
+      userId,
+      newEmail,
+      deviceInfo,
+    });
+
+    return res.status(202).json({
+      message: "Email change OTP sent",
+      pendingEmail,
+      emailChangeOtpExpireAt,
+      emailChangeOtpResendAvailableAt,
+    });
+  },
+);
+
+const resendEmailChange = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { id: userId } = req.user;
+    const deviceInfo = getDeviceInfo(req);
+
+    const {
+      emailChangeOtpExpireAt,
+      emailChangeOtpResendAvailableAt,
+      pendingEmail,
+    } = await resendEmailChangeService({
+      userId,
+      deviceInfo,
+    });
+
+    return res.status(202).json({
+      message: "Successfully sent another OTP to your new email address",
+      pendingEmail,
+      emailChangeOtpExpireAt,
+      emailChangeOtpResendAvailableAt,
+    });
+  },
+);
+
+const verifyEmailChange = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { id: userId } = req.user;
+    const { otp } = req.body;
+    const deviceInfo = getDeviceInfo(req);
+
+    const { user } = await verifyEmailChangeService({
+      userId,
+      otp,
+      deviceInfo,
+    });
+
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.status(200).json({
+      message: "Successfully changed email, please sign in again",
+      user,
+    });
+  },
+);
+
 export {
   updateProfilePicture,
   deleteProfilePicture,
@@ -122,4 +200,7 @@ export {
   getNotificationSettings,
   updateNotificationSettings,
   markNotificationsAsSeen,
+  sendEmailChange,
+  resendEmailChange,
+  verifyEmailChange,
 };
