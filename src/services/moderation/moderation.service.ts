0e52a294289a4d58860bfc2d4f0aeb7e2db489d3
Kelsen23 <nikolozigoreziani@gmail.com>
2026-06-15 15:29:22 +0400

    feat(moderation.service): create a service for exporting all http controller-called services

---
+++ src/services/moderation/moderation.service.ts
@@ -0,0 +1,4 @@
+export { default as createReport } from "./createReport.service.js";
+export { default as moderate } from "./moderate.service.js";
+export { default as getBan } from "./getBan.service.js";
+export { default as moderateContentImage } from "./requestContentImageModeration.service.js";
