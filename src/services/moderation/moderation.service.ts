7193dd0a5dd7b41eb69d3576a2da121f0ffffcc5
Kelsen23 <nikolozigoreziani@gmail.com>
2026-06-18 21:44:58 +0400

    feat(moderation.service): export sendBanNoticeEmail service

--- src/services/moderation/moderation.service.ts
+++ src/services/moderation/moderation.service.ts
@@ -2,3 +2,4 @@ export { default as createReport } from "./createReport.service.js";
 export { default as moderate } from "./moderate.service.js";
 export { default as getBan } from "./getBan.service.js";
 export { default as moderateContentImage } from "./requestContentImageModeration.service.js";
+export { default as sendBanNoticeEmail } from "./sendBanNoticeEmail.service.js";
