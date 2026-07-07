417325a569610875e0afda9320932dec93bb57a5
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-07 16:19:02 +0400

    refactor(imageDeletion.service): remove the option of deleting all images from the body

--- src/services/media/worker/imageDeletion.service.ts
+++ src/services/media/worker/imageDeletion.service.ts
@@ -1,24 +1,12 @@
-import deleteImagesFromBody from "../../media/deleteImageFromBody.service.js";
 import deleteSingleImage from "../../media/deleteSingleImage.service.js";
 
 const processImageDeletionJob = async (
   jobName: string,
-  jobData:
-    | { objectKey: string }
-    | { body: string; entityType: "answer" | "question"; entityId: string },
+  jobData: { objectKey: string },
 ) => {
   switch (jobName) {
     case "DELETE_SINGLE":
-      await deleteSingleImage(jobData as { objectKey: string });
-      break;
-    case "DELETE_FROM_BODY":
-      await deleteImagesFromBody(
-        jobData as {
-          body: string;
-          entityType: "answer" | "question";
-          entityId: string;
-        },
-      );
+      await deleteSingleImage(jobData);
       break;
     default:
       throw new Error("Invalid job type");
