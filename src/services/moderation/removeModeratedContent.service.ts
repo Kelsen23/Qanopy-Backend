fa3e68caaaf9f8a702fd5a9df7d2196c1a67b89c
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-07 16:22:56 +0400

    refactor(removeModeratedContent.service): get rid of non-existent DELETE_FROM_BODY imageDeletionQueue enqueues

--- src/services/moderation/removeModeratedContent.service.ts
+++ src/services/moderation/removeModeratedContent.service.ts
@@ -6,14 +6,11 @@ import {
   clearVersionHistoryCache,
 } from "../../utils/cache/clearCache.util.js";
 import { getContentTypeLabel } from "../../utils/content/contentTypeLabel.util.js";
-import { makeJobId } from "../../utils/job/makeJobId.util.js";
 
 import Question from "../../models/question.model.js";
 import Answer from "../../models/answer.model.js";
 import Reply from "../../models/reply.model.js";
 import AiAnswerFeedback from "../../models/aiAnswerFeedback.model.js";
-
-import imageDeletionQueue from "../../queues/imageDeletion.queue.js";
 
 type ModeratedTargetType =
   | "QUESTION"
@@ -78,27 +75,6 @@ const removeModeratedContent = async (
   await Model.findByIdAndUpdate(foundContent._id || foundContent.id, {
     $set: { isActive: false },
   });
-
-  if (typeof foundContent.body === "string" && foundContent.body.length > 0) {
-    await imageDeletionQueue.add(
-      "DELETE_FROM_BODY",
-      {
-        body: foundContent.body,
-        entityType: targetType,
-        entityId: targetId,
-      },
-      {
-        removeOnComplete: true,
-        removeOnFail: false,
-        jobId: makeJobId(
-          "imageDeletion",
-          "DELETE_FROM_BODY",
-          targetType,
-          targetId,
-        ),
-      },
-    );
-  }
 
   if (targetType === "QUESTION") {
     await getRedisCacheClient().del(`question:${targetId}`);
