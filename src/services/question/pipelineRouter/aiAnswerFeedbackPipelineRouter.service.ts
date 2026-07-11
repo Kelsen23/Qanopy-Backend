0526e3e0946f5dfff2d32c5ef2b44c0c6b1a7bc0
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-11 10:58:07 +0400

    refactor(aiAnswerFeedbackPipelineRouter.service): implement new services/utils and make the service more maintainable

--- src/services/question/pipelineRouter/aiAnswerFeedbackPipelineRouter.service.ts
+++ src/services/question/pipelineRouter/aiAnswerFeedbackPipelineRouter.service.ts
@@ -1,42 +1,18 @@
-import { makeJobId } from "../../../utils/job/makeJobId.util.js";
-
 import AiAnswerFeedback from "../../../models/aiAnswerFeedback.model.js";
 
-import contentModerationQueue from "../../../queues/contentModeration.queue.js";
+import { routePendingModerationContent } from "./moderationPipelineRouter.shared.js";
 
 const aiAnswerFeedbackPipelineRouter = async (
   aiAnswerFeedbackId: string,
   moderationRevision?: number,
 ) => {
-  const foundAiAnswerFeedback = await AiAnswerFeedback.findById(
-    aiAnswerFeedbackId,
-  ).select("_id moderationStatus moderationRevision");
-
-  if (
-    !foundAiAnswerFeedback ||
-    foundAiAnswerFeedback.moderationStatus !== "PENDING" ||
-    (moderationRevision !== undefined &&
-      foundAiAnswerFeedback.moderationRevision !== moderationRevision)
-  )
-    return;
-
-  await contentModerationQueue.add(
-    "AI_ANSWER_FEEDBACK",
-    {
-      contentId: foundAiAnswerFeedback._id,
-      moderationRevision: foundAiAnswerFeedback.moderationRevision,
-    },
-    {
-      removeOnComplete: true,
-      removeOnFail: false,
-      jobId: makeJobId(
-        "contentModeration",
-        "AI_ANSWER_FEEDBACK",
-        foundAiAnswerFeedback._id,
-        foundAiAnswerFeedback.moderationRevision,
-      ),
-    },
-  );
+  await routePendingModerationContent({
+    contentType: "AI_ANSWER_FEEDBACK",
+    contentId: aiAnswerFeedbackId,
+    moderationRevision,
+    model: AiAnswerFeedback,
+    select: "_id moderationStatus moderationRevision",
+  });
 };
 
 export default aiAnswerFeedbackPipelineRouter;
