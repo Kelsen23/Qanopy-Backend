7af74889ba9d09ed01634a7bb3b85e7d2d0b0596
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-11 10:57:43 +0400

    refactor(replyPipelineRouter.service): implement new services/utils and make the service more maintainable

--- src/services/question/pipelineRouter/replyPipelineRouter.service.ts
+++ src/services/question/pipelineRouter/replyPipelineRouter.service.ts
@@ -1,42 +1,18 @@
-import { makeJobId } from "../../../utils/job/makeJobId.util.js";
-
 import Reply from "../../../models/reply.model.js";
 
-import contentModerationQueue from "../../../queues/contentModeration.queue.js";
+import { routePendingModerationContent } from "./moderationPipelineRouter.shared.js";
 
 const replyPipelineRouter = async (
   replyId: string,
   moderationRevision?: number,
 ) => {
-  const foundReply = await Reply.findById(replyId).select(
-    "_id moderationStatus moderationRevision",
-  );
-
-  if (
-    !foundReply ||
-    foundReply.moderationStatus !== "PENDING" ||
-    (moderationRevision !== undefined &&
-      foundReply.moderationRevision !== moderationRevision)
-  )
-    return;
-
-  await contentModerationQueue.add(
-    "REPLY",
-    {
-      contentId: foundReply._id,
-      moderationRevision: foundReply.moderationRevision,
-    },
-    {
-      removeOnComplete: true,
-      removeOnFail: false,
-      jobId: makeJobId(
-        "contentModeration",
-        "REPLY",
-        foundReply._id,
-        foundReply.moderationRevision,
-      ),
-    },
-  );
+  await routePendingModerationContent({
+    contentType: "REPLY",
+    contentId: replyId,
+    moderationRevision,
+    model: Reply,
+    select: "_id moderationStatus moderationRevision",
+  });
 };
 
 export default replyPipelineRouter;
