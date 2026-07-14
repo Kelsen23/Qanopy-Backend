adcab30c44ebe4d7d4af01fdc95abb70c6bf8214
Kelsen23 <nikolozigoreziani@gmail.com>
2026-07-14 14:34:27 +0400

    feat(eligibilityGateActionLog.model): rename eligibilityGateActionLog model and add more fields to it

---
+++ src/models/eligibilityGateActionLog.model.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+
+const EligibilityGateActionLogSchema: Schema = new Schema(
+  {
+    decisionId: { type: String, required: true, unique: true, index: true },
+    questionId: {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+      index: true,
+    },
+    version: { type: Number, required: true, min: 1, index: true },
+    userId: { type: String, required: true, index: true },
+
+    decision: {
+      type: String,
+      enum: ["ALLOW", "CLARIFY", "REJECT"],
+      required: true,
+    },
+    questionEligibilityStatus: {
+      type: String,
+      enum: ["ALLOWED", "CLARIFY", "REJECTED"],
+      required: true,
+    },
+    securityVerifierStatus: {
+      type: String,
+      enum: ["NOT_REQUIRED", "PENDING"],
+      required: true,
+    },
+    eligibleForDownstreamProcessing: { type: Boolean, required: true },
+
+    userFacingReason: { type: String, required: true },
+    internalReason: { type: String, required: true },
+    metadata: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {},
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: (_, ret: any) => {
+        ret.id = ret._id;
+
+        delete ret._id;
+
+        return ret;
+      },
+    },
+  },
+);
+
+export default mongoose.model(
+  "EligibilityGateActionLog",
+  EligibilityGateActionLogSchema,
+  "question_eligibility_gate_action_logs",
+);
